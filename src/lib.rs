0e2e4fe8045eef228547628334f5cdb4141c963e
TyOverby <ty@pre-alpha.com>
2014-11-20 10:56:03 -0800

    implemented client-side connect

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,52 @@
-#[test]
-fn it_works() {
+extern crate bincode;
+extern crate serialize;
+
+use std::io::net::tcp::TcpStream;
+use std::io::{IoResult, IoError, BufferedReader};
+use std::comm::{Sender, Receiver, channel};
+use std::task::spawn;
+
+use serialize::{Decodable, Encodable};
+
+use bincode::{EncoderWriter, DecoderReader};
+
+pub fn connect<'a, 'b, I, O>(ip: &str, port: u16) -> IoResult<(Receiver<I>, Sender<O>)>
+where I: Send + Decodable<DecoderReader<'a, BufferedReader<TcpStream>>, IoError>,
+      O: Send + Encodable<EncoderWriter<'b, TcpStream>, IoError> {
+    let path = format!("{}:{}", ip, port);
+    Ok(upgrade(try!(TcpStream::connect(path[]))))
+}
+
+pub fn upgrade<'a, 'b, I, O>(stream: TcpStream) -> (Receiver<I>, Sender<O>)
+where I: Send + Decodable<DecoderReader<'a, BufferedReader<TcpStream>>, IoError>,
+      O: Send + Encodable<EncoderWriter<'b, TcpStream>, IoError> {
+    let (in_snd, in_rec) = channel();
+    let (out_snd, out_rec) = channel();
+
+    let s1 = stream.clone();
+    let s2 = stream.clone();
+
+    // "in" thread.
+    spawn(proc() {
+        let mut s1 = BufferedReader::new(s1);
+        loop {
+            match bincode::decode_from(&mut s1) {
+                Ok(a) => in_snd.send(a),
+                Err(_) => return
+            }
+        }
+    });
+    // "out" thread.
+    spawn(proc() {
+        let mut s2 = s2;
+        loop {
+            let obj = out_rec.recv();
+            match bincode::encode_into(&obj, &mut s2) {
+                Ok(()) => {},
+                Err(_) => return
+            }
+        }
+    });
+
+    (in_rec, out_snd)
 }
